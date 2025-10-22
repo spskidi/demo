3e4cabc2d589bd29dd18af3c6ca341e1eee532fc
spskidi <suryaprakash0923@gmail.com>
2025-10-22 11:04:04 +0530

    Update WSGI configuration and clean up imports

--- edtech_project/wsgi.py
+++ edtech_project/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Add the project directory to the Python path
+project_path = Path(__file__).parent
+if str(project_path) not in sys.path:
+    sys.path.append(str(project_path))
 
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edtech_project.settings')
 
+# This application object is used by the development server and any WSGI server
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
