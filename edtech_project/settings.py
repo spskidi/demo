3723ee4d05ca6336fe8ebe84f6ff67daef9b1ed1
spskidi <suryaprakash0923@gmail.com>
2025-10-22 11:01:25 +0530

    Remove dj_database_url import and clean up database configuration

--- edtech_project/settings.py
+++ edtech_project/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
-import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 import sys
@@ -95,21 +94,13 @@ WSGI_APPLICATION = 'edtech_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Use SQLite for development
+# Database configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# For production, you can uncomment and use this PostgreSQL configuration
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-#         conn_max_age=600
-#     )
-# }
 
 
 # Password validation
